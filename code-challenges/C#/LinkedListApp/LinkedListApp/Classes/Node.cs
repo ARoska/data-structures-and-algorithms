e882792e92d67c022873822b9a82177a0b66be27
Andrew Roska <andrew.l.roska@gmail.com>
2019-03-25 11:56:00 -0700

    Adds solution to challenge.

---
+++ code-challenges/C#/LinkedListApp/LinkedListApp/Classes/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListApp.Classes
+{
+    public class Node
+    {
+        public Node next;
+
+        public int data;
+    }
+}
---
+++ code-challenges/C#/LinkedListApp/LinkedListApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using LinkedListApp.Classes;
+
+namespace LinkedListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            int counter = 47;
+
+            linkedList.Insert(counter);
+            counter++;
+            linkedList.Insert(counter);
+            counter++;
+            linkedList.Insert(counter);
+            counter++;
+            linkedList.Insert(counter);
+            counter++;
+            linkedList.Insert(counter);
+            counter++;
+            linkedList.Insert(counter);
+            counter++;
+            linkedList.Insert(counter);
+            counter++;
+
+            linkedList.PrintAllNodes();
+
+            int[] actualReturn = linkedList.RertunAllNodes();
+            Console.WriteLine("[{0}]", string.Join(", ", actualReturn));
+        }
+    }
+}
