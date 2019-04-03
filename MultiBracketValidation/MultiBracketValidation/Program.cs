a3fa37a8068029d7e772f20d28f9339cd130f106
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-03 15:17:34 -0700

    Creates method.

---
+++ MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,71 @@
+﻿using StacksAndQueues.Classes;
+using System;
+
+namespace MultiBracketValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool MultiBracketValidation(string str)
+        {
+            MyStack<char> myStack = new MyStack<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '{' || str[i] == '[' || str[i] == '(')
+                {
+                    Node<char> node = new Node<char>(str[i]);
+                    myStack.Push(node);
+                }
+                if (str[i] == '}' || str[i] == ']' || str[i] == ')')
+                {
+                    char character = str[i];
+                    switch (character)
+                    {
+                        case '}':
+                            if (Compare(character, myStack) == false)
+                            {
+                                return false;
+                            }
+                            break;
+
+                        case ']':
+                            if (Compare(character, myStack) == false)
+                            {
+                                return false;
+                            }
+                            break;
+
+                        case ')':
+                            if (Compare(character, myStack) == false)
+                            {
+                                return false;
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool Compare(char character, MyStack<char> myStack)
+        {
+            Node<char> node = myStack.Peek();
+            char top = node.Value;
+
+            if (top == character)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
