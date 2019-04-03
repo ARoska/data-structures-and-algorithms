29a315817ada6baee8312d3a3dd3430ea09d683c
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-03 07:52:36 -0700

    Create Overloads for Node.

--- data-structures/C#/StacksAndQueues/StacksAndQueues/Classes/Node.cs
+++ data-structures/C#/StacksAndQueues/StacksAndQueues/Classes/Node.cs
@@ -6,14 +6,28 @@ namespace StacksAndQueues.Classes
 {
     public class Node<T> where T : IConvertible
     {
-        public T Value { get; set; }
-
         public Node<T> Next { get; set; }
-        public int Timestamp{ get; set; }
+
+        public T Value { get; set; }
+        public int Timestamp { get; set; }
+        public string Name { get; set; }
 
         public Node(T value)
         {
             Value = value;
+        }
+
+        public Node(T value, int timestamp)
+        {
+            Value = value;
+            Timestamp = timestamp;
+        }
+
+        public Node(T value, int timestamp, string name)
+        {
+            Value = value;
+            Timestamp = timestamp;
+            Name = name;
         }
     }
 }
