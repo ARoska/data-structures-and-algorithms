9b34d3acf9e983604a4c0a86edfe75d7e95bbf26
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-10 14:15:23 -0700

    Creates method and updates README

---
+++ code-challenges/C#/FindMaximumBinaryTree/FindMaximumBinaryTree/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree.Classes;
+
+namespace FindMaximumBinaryTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int FindMaximumValue(MyBinaryTree<int> tree)
+        {
+            int maxValue = 0;
+            Queue<Node<int>> queue = new Queue<Node<int>>();
+            queue.Enqueue(tree.Root);
+
+            while (queue.Count != 0)
+            {
+                Node<int> temp = queue.Dequeue();
+
+                if (temp.LChild != null)
+                {
+                    queue.Enqueue(temp.LChild);
+                }
+                if (temp.RChild != null)
+                {
+                    queue.Enqueue(temp.RChild);
+                }
+                if (temp.Value > maxValue)
+                {
+                    maxValue = temp.Value;
+                }
+            }
+
+            return maxValue;
+        }
+    }
+}
---
+++ code-challenges/C#/FindMaximumBinaryTree/README.md
@@ -0,0 +1,28 @@
+# Find Max Value in Binary Tree
+#### *Author: Andrew Roska*
+
+------------------------------
+
+## Description
+A simple method that takes in a `Binary Tree` with `int` values and returns the largest value in the the `BT`.  The `BT` is traversed with a breadth-first approach.
+
+------------------------------
+
+## Methods
+
+| Method | Summary | Big O Time | Big O Space | Example | 
+| :----------- | :----------- | :-------------: | :-------------: | :----------- |
+| FindMaximumValue | Returns the larges `int` in the `BT` | O(n) | O(w) w = width of the `BT` | myTree.FindMaximumValue() |
+
+
+------------------------------
+
+## Visuals
+##### Find Maximum Value Whiteboard
+![Find Maximum Value Whiteboard](https://github.com/Roketsu86/data-structures-and-algorithms/blob/master/assets/MaxValueTree/FindMaximumValue-WB.jpg)
+
+------------------------------
+
+## Change Log
+1.0: *Initial Build* - 10 April 2019
+
