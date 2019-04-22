a3d348f32c204ba99b75632da70c8458c5d26908
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-22 07:55:45 -0700

    Impliments InsertionSort method.

--- sorts/C#/Sorts/Sorts/Program.cs
+++ sorts/C#/Sorts/Sorts/Program.cs
@@ -9,9 +9,23 @@ namespace Sorts
             Console.WriteLine("Hello World!");
         }
 
-        public static void InsertionSort()
+        public static int[] InsertionSort(int[] arr)
         {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int temp = arr[i];
+                int j = i - 1;
 
+                while (j >= 0 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = temp;
+            }
+
+            return arr;
         }
     }
 }
