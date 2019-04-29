4dc7f88de0c8fc01c4fa210b04e0ce699780aeeb
Andrew Roska <andrew.l.roska@gmail.com>
2019-04-29 10:16:58 -0700

    Adds HashTable methods.

--- data-structures/C#/Hashtable/HashTests/UnitTest1.cs
+++ data-structures/C#/Hashtable/HashTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using Hashtable.Classes;
 
 namespace HashTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanAddKeyValuePairToHashTable()
         {
+            MyHashTable hashTable = new MyHashTable();
+            MyNode expectedNode = new MyNode("test", "test");
 
+            hashTable.Add("test", "test");
+            MyNode resultNode = hashTable.Get("test");
+
+            Assert.Equal(resultNode.Key, expectedNode.Key);
+            Assert.Equal(resultNode.Value, expectedNode.Value);
         }
     }
 }
