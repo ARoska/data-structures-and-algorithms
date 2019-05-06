d61a258f4df0a4479a239b1bf4fa0473bc9062d6
Andrew Roska <andrew.l.roska@gmail.com>
2019-05-06 10:07:31 -0700

    Adds Graph classes.

---
+++ data-structures/C#/Graph/Graph/Classes/Edge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph.Classes
+{
+    public class Edge<T>
+    {
+        public int Weight { get; set; }
+        public Node<T> Node { get; set; }
+
+        public Edge(Node<T> node, int weight)
+        {
+            Node = node;
+            Weight = weight;
+        }
+    }
+}
---
+++ data-structures/C#/Graph/GraphTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Graph.Classes;
+
+namespace GraphTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
