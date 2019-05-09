dcfafccf647dca66521131ce578e62f3a63dcb16
Andrew Roska <andrew.l.roska@gmail.com>
2019-05-09 10:52:05 -0700

    Adds depth-first traversal method.

--- data-structures/C#/Graph/Graph/Classes/Neighbors.cs
+++ data-structures/C#/Graph/Graph/Classes/Neighbors.cs
@@ -1,6 +1,9 @@
-﻿namespace Graph.Classes
+﻿using System.Collections.Generic;
+
+namespace Graph.Classes
 {
     public class Neighbors
     {
+        public static Dictionary<Node<T>, int> AllNeighbors { get; set; }
     }
 }
